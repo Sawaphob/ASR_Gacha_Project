9c6de9a1c6e26ee83bea74fb8b7f97dd4b0a30e3
Roneaw <ronun15_6c-crazy@hotmail.com>
2018-04-30 23:56:40 +0700

    video initial animation v.2

--- src/page/Start.js
+++ src/page/Start.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react'
 import Money from '../component/Money'
 import Token from '../component/Token'
 import Collection from '../component/Collection'
+import Video from '../component/Video'
+import Microphone from '../component/Microphone'
 import {Col,Row} from 'react-bootstrap'
+import myWorker from "../lib/recorderWorker.js"
+//let myWorker = require("../lib/recorderWorker.js");
 
 export default class Start extends Component{
     render(){
         return(
             <div>
+                <div>
+                <Row>
+                    <Microphone worker={myWorker} />
+                </Row>
                 <Row>
                     <Col xs={1}>
                         <Token />
@@ -16,9 +24,15 @@ export default class Start extends Component{
                         <Money />
                     </Col>
                 </Row>
-                <Row>
-                    <Collection />
-                </Row>
+                </div>
+                <div style={{position: 'static'}}>
+                    <Video/>
+                </div>
+                <div style={{position: 'static'}}>
+                    <Row>
+                        <Collection />
+                    </Row>
+                </div>        
             </div>
         )
     }
