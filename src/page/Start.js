7a274369445b3e9ae5357fbd492e18afdb1ad57b
Attawit Chaiyaroj <ahm.913@hotmail.com>
2018-05-02 00:37:13 +0700

    Separated gachaResolve as a JSON

--- src/page/Start.js
+++ src/page/Start.js
@@ -7,38 +7,7 @@ import Microphone from '../component/Microphone'
 import {Col,Row} from 'react-bootstrap'
 import myWorker from "../lib/recorderWorker.js"
 //let myWorker = require("../lib/recorderWorker.js");
-
-const gachaResolve = {
-    "0" : "atiwong_asr.jpg",
-    "1" : "atiwong1_n.jpg",
-    "2" : "atiwong1_r.jpg",
-    "3" : "atiwong1_sr.jpg",
-    "4" : "atiwong2_asr.jpg",
-    "5" : "atiwong2_n.jpg",
-    "6" : "atiwong2_r.jpg",
-    "7" : "atiwong2_sr.jpg",
-    "8" : "atiwong3_n.jpg",
-    "9" : "atiwong3_r.jpg",
-    "10" : "atiwong3_sr.jpg",
-    "11" : "atthasit_asr.jpg",
-    "12" : "ekapol_asr.jpg",
-    "13" : "ekapol1_n.jpg",
-    "14" : "ekapol1_r.jpg",
-    "15" : "ekapol1_sr.jpg",
-    "16" : "ekapol2_n.jpg",
-    "17" : "ekapol2_r.jpg",
-    "18" : "ekapol2_sr.jpg",
-    "19" : "ekapol3_n.jpg",
-    "20" : "ekapol3_r.jpg",
-    "21" : "ekapol3_sr.JPG",
-    "22" : "ekapol4_n.jpg",
-    "23" : "ekapol4_r.jpg",
-    "24" : "ekapol5_n.jpg",
-    "25" : "ekapol5_r.jpg",
-    "26" : "ekapol6_n.jpg",
-    "27" : "ekapol6_r.jpg",
-    "28" : "ekapolAndatiwong_asr.jpg"
-}
+import gachaResolve from '../data/gachaResolve.json'
 
 export default class Start extends Component{
 
